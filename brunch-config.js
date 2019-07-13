c58e98bd0a2ef35ea3c4a7842417aa85cbb67728
Euregan <va.mcxp@gmail.com>
2019-07-13 17:42:41 +0200

    Added a bundler to compile JS and CSS files

---
+++ brunch-config.js
@@ -0,0 +1,13 @@
+exports.paths = {
+  watched: ['src', 'public/css'],
+  public: 'dist'
+}
+
+exports.files = {
+  javascripts: {
+    joinTo: 'kanshi.js'
+  },
+  stylesheets: {
+    joinTo: 'kanshi.css'
+  }
+}
